984fdbc98c532dd0efc06e1a866d2f21a3954082
manjitha-24 <85818073+manjitha-24@users.noreply.github.com>
2023-08-12 20:25:22 +0530

    chatbot_api.js

--- chatbot_api.js
+++ chatbot_api.js
@@ -1,8 +1,1 @@
-const request = require("request");
 
-request(
-    "http://www.omdbapi.com/?i=tt3896198&apikey=5b0f1c19",
-    function (err, responce, body){
-        console.log(body);
-    }
-);
